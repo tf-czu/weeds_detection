fc3699dc2566b02ecc4e3b9102822ee394c90296
tajgr <levjakub@gmail.com>
2023-02-22 15:31:50 +0100

    label_weeds.py: remove image processing

--- label_weeds.py
+++ label_weeds.py
@@ -6,55 +6,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-g_threshold = 160
-
-
-def transform_nir(nir):
-    n, m = nir.shape
-    nir_resized = cv2.resize(nir, (m - 6, n - 6))
-    ret_nir = np.zeros((n, m), np.uint8)
-    ret_nir[3:-3, 3:-3] = nir_resized
-
-    return ret_nir
-
-
-def get_ndvi_im(im_color, nir):
-    b, g, r = cv2.split(im_color)
-    r = r.astype(float)
-    nir = nir.astype(float)
-    ndvi = (nir - r) / (nir + r)
-    ndvi_im = (ndvi + 1)/2 * 255
-
-    return ndvi_im.astype(np.uint8)
-
-
-def show_im(im):
-    cv2.namedWindow("win", cv2.WINDOW_NORMAL)
-    n, m = im.shape
-    cv2.resizeWindow("win", m // 2, n // 2)
-    cv2.imshow("win", im)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-def show_hist(im):
-    plt.hist(im.ravel(), 256, [0, 256])
-    plt.show()
-
-
-def test_ndvi(path):
-    assert os.path.isfile(path)
-    assert path.endswith("C.tif")
-    color_im = cv2.imread(path)
-
-    nir_path = path[:-5]+"N.tif"
-    assert os.path.exists(nir_path)
-    nir_im = cv2.imread(nir_path, cv2.IMREAD_GRAYSCALE)
-
-    ndvi = get_ndvi_im(color_im, nir_im)
-    show_im(ndvi)
-    show_hist(ndvi)
-    ret, binary_im = cv2.threshold(ndvi, g_threshold, 255, cv2.THRESH_BINARY)
-    show_im(binary_im)
+from image_processing import transform_nir, get_ndvi_im
 
 
 class LabelWeeds:
@@ -101,11 +53,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='__doc__')
     parser.add_argument('path', help='Path to image directory.')
-    parser.add_argument('--ndvi', action="store_true")
 
     args = parser.parse_args()
-    if args.ndvi:
-        test_ndvi(args.path)
-    else:
-        label = LabelWeeds(path = args.path)
-        label.run()
+    label = LabelWeeds(path = args.path)
+    label.run()
