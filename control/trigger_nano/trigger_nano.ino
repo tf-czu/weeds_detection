c66737b2f3749bb07d7547d5f4d1fec70da55461
tajgr <levjakub@gmail.com>
2023-04-18 12:53:30 +0200

    gger_nano.ino: add variable delay, work in progress

--- control/trigger_nano/trigger_nano.ino
+++ control/trigger_nano/trigger_nano.ino
@@ -1,5 +1,6 @@
 String inputString = "";
 bool stringComplete = false;
+long delayVal = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -14,11 +15,25 @@ void setup() {
 void loop() {
   if (stringComplete) {
     Serial.println(inputString);
+    //delayVal = inputString.toInt();
+    //if (delayVal == 0) {
+      //delayVal = 1000;  // default time in us
+      //Serial.println(0);
+    //}
+    //else if (delayVal > 160) {
+      delayVal = 16000;
+      //Serial.println(delayVal);
+    //}
+    //else {
+      //delayVal = delayVal*100;
+      //Serial.println(delayVal);
+    //}
     digitalWrite(4, HIGH);  // LED
     digitalWrite(5, HIGH);  // camera trigger
-    delay(1);
-    digitalWrite(4, LOW);  // LED
+    delayMicroseconds(100);
     digitalWrite(5, LOW);  // camera trigger
+    //delayMicroseconds(delayVal-100+1);   // what happen if the result is 0?
+    digitalWrite(4, LOW);  // LED
     // clear the string:
     inputString = "";
     stringComplete = false;
@@ -26,16 +41,18 @@ void loop() {
 
 }
 
-void serialEvent() {
+void serialEvent(char string) {
   while (Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read();
+    // Serial.println(inChar);
     // add it to the inputString:
     inputString += inChar;
     // if the incoming character is a newline, set a flag so the main loop can
     // do something about it:
     if (inChar == '\n') {
       stringComplete = true;
+      // Serial.println(inputString);
     }
   }
 }
