f0dd5ec4617e83198d675bd6ffb76569851e527f
tajgr <levjakub@gmail.com>
2023-04-19 18:15:20 +0200

    take_trigger.py: fixup, working version

--- control/take_trigger.py
+++ control/take_trigger.py
@@ -8,21 +8,19 @@ import time
 
 class TakeTrigger:
     def __init__(self, port, trig_interval, led_duration):
-        assert type(trig_interval) == int or float, trig_interval
-        assert type(led_duration) == int or float, led_duration
         assert led_duration >= 0.1 and led_duration <= 16, led_duration
         self.trig_interval = trig_interval
         self.msg = f"{int(led_duration*10)}\n"
         self.read_timeout = 0.1
         self.s = serial.Serial(port, 115200)
-        time.sleep(0.5)
+        time.sleep(2)
         print(self.serial_read())
 
     def serial_read(self):
         t0 = time.time()
         data = b""
         while True:
-            d = self.s.read(16)
+            d = self.s.read(1)
             if len(d)>0:
                 data += d
             if b"\n" in data:
@@ -55,15 +53,15 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--interval', '-i', help='Time interval between trigger (s).', default=1)
+    parser.add_argument('--interval', '-i', help='Time interval between trigger (s).', type=float, default=1)
     parser.add_argument('--led-duration', '-l',
-                        help='LED light duration (ms), values: 0.1 - 16 ms', default=1)
+                        help='LED light duration (ms), values: 0.1 - 16 ms', type=float, default=1)
     parser.add_argument('--port', '-p', help='Serial port, default: /dev/ttyUSB0', default="/dev/ttyUSB0")
     args = parser.parse_args()
 
     trig_interval = args.interval
     led_duration = args.led_duration
-    print(f"Set trigger interval: {trig_interval} s, LED light duration: {led_duration/10} ms")
+    print(f"Set trigger interval: {trig_interval} s, LED light duration: {led_duration} ms")
 
     with TakeTrigger(args.port, trig_interval, led_duration) as take_trigger:
         take_trigger.run()
