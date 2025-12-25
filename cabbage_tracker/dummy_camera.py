7a6560d62cb2da9d191b59ecacd4591bdab1eeb4
tajgr <levjakub@gmail.com>
2025-12-25 22:21:52 +0100

    dummy_camera.py: initial commit, working ver

---
+++ cabbage_tracker/dummy_camera.py
@@ -0,0 +1,62 @@
+"""
+    Simulate camera using images stored in a directory
+"""
+
+import os
+import logging
+import time
+from threading import Thread
+
+import cv2
+
+g_logger = logging.getLogger(__name__)
+
+class DummyCamera:
+    def __init__(self, config, bus):
+        self.bus = bus
+        self.bus.register("image")
+        self.interval = 1.0 / config.get("frequency_hz", 5)
+        self.im_dir = config.get("directory")
+        if self.im_dir is None:
+            g_logger.error(f"No directory was specified!")
+
+        self.last_im_time = None
+        self.input_thread = Thread(target=self.run_input, daemon=True)
+
+    def start(self):
+        self.input_thread.start()
+
+    def join(self, timeout=None):
+        self.input_thread.join(timeout=timeout)
+
+    def load_images(self):
+        names = sorted(os.listdir(self.im_dir))
+        ret_list = []
+        for name in names:
+            im_path = os.path.join(self.im_dir, name)
+            if os.path.isfile(im_path) and name.endswith(tuple(["tiff", "tif", "png", "jpg", "jpeg"])):
+                ret_list.append(im_path)
+        return ret_list
+
+    def run_input(self):
+        if self.im_dir:
+            images_paths = self.load_images()
+            n = len(images_paths)
+            assert n != 0, images_paths
+            ii = 0
+            while self.bus.is_alive():
+                img = cv2.imread(images_paths[ii])
+                if self.last_im_time:
+                    dt = time.time() - self.last_im_time
+                    self.bus.sleep(self.interval - dt)
+                self.last_im_time = time.time()
+                self.bus.publish("image", img)
+                ii += 1
+                if ii == n:
+                    ii = 0
+
+        else:
+            self.request_stop()
+
+    def request_stop(self):
+        self.bus.shutdown()
